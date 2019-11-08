46dcbe035c0c3406835da3b9f9d66f7b0a1b9db7
Pablo <pablomanuelcoronel@gmail.com>
2019-11-08 15:57:11 -0300

    funciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app-prueba';
+  title = "Angular";
+  numeros: number[] = [1, 2, 3, 4, 5];
+
+  nombres: string[] = [
+    "diego",
+    "pablo",
+    "miguel",
+    "alejandro",
+    "roxana",
+    "alison",
+    "lucas",
+    "david"
+  ];
+
+  isActive: boolean = false;
+
+  // funcion
+  public esPar(numero: number): boolean {
+    return numero % 2 == 0 ? true : false;
+  }
+
+  public sumarNumero(): void {
+    this.numeros.push(this.numeros[this.numeros.length - 1] + 1);
+  }
+
+  public toggle() {
+    this.isActive = !this.isActive;
+  }
 }
